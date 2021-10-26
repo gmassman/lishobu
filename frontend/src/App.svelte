621441a577cfcd757a5bf44dc0c85c4e1b3645d2
Garrett Massman <gmassman20@gmail.com>
2021-10-25 21:57:53 -0700

    Use axios to GET from backend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,65 +1,32 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+    import axios from 'axios'
+
+    let api_host: string = import.meta.env.VITE_API_HOST as string
+
+    let data: string = ""
+    const getData = async () => {
+        const resp = await axios.get(api_host)
+        data = resp.data
+    }
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world</h1>
+    API at {api_host}
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+    <button on:click={getData}>Backend says</button>
+    {data}
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
+    :root {
+        font-family: Helvetica, Verdana, Arial, Geneva, sans-serif;
     }
 
-    p {
-      max-width: none;
+    button {
+        margin-top: 0.5em;
+        margin-bottom: 0.5em;
+        display: block;
+        font-size: 1.5em;
+        border-radius: 0.25em;
     }
-  }
 </style>
